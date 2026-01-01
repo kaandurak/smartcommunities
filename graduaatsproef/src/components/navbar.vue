5eea62562422369f6a846b64c70cfe4a3d25a59a
kaandurak <kaandurak3d@gmail.com>
2026-01-01 16:42:21 +0100

    testing with routers

--- graduaatsproef/src/components/navbar.vue
+++ graduaatsproef/src/components/navbar.vue
@@ -1,16 +1,20 @@
 <script setup>
-
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <div class="bg-white grid grid-cols-12 p-7 shadow-2xl mb-7">
-    <div class="w-[15rem] h-auto col-start-2 col-span-2">
+    <div class="w-60 h-auto col-start-2 col-span-2">
         <img src="../assets/logo_pxl_smart-ict.svg" alt="pxl-logo">
     </div>
   </div>
 
 
   <div class="grid grid-cols-12">
+
+    <RouterLink to="/eventview">
+        Event
+    </RouterLink>
 
     <div class="col-start-2 col-span-10 flex justify-between">
          <button class="bg-white border-gray-400 border-4 rounded  text-black w-[45%] p-4 focus:bg-gray-600 focus:text-white ">
--- graduaatsproef/src/router/index.js
+++ graduaatsproef/src/router/index.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Event from '@/views/eventview.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
   {
-
+    path:'/eventview',
+    name:'eventview',
+    component:Event
   },
 
   ],
