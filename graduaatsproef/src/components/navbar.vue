2463d50d7981db18290d19deef84aa73f7ac5e8e
kaandurak <kaandurak3d@gmail.com>
2026-01-01 16:22:41 +0100

    navbar design v1.

--- graduaatsproef/src/components/navbar.vue
+++ graduaatsproef/src/components/navbar.vue
@@ -3,5 +3,24 @@
 </script>
 
 <template>
+  <div class="bg-white grid grid-cols-12 p-7 shadow-2xl mb-7">
+    <div class="w-[15rem] h-auto col-start-2 col-span-2">
+        <img src="../assets/logo_pxl_smart-ict.svg" alt="pxl-logo">
+    </div>
+  </div>
+
+
+  <div class="grid grid-cols-12">
+
+    <div class="col-start-2 col-span-10 flex justify-between">
+         <button class="bg-white border-gray-400 border-4 rounded  text-black w-[45%] p-4 focus:bg-gray-600 focus:text-white ">
+        Comunnities
+    </button>
+
+    <button  class="bg-white border-gray-400 border-4 rounded  text-black w-[45%] p-4 focus:bg-gray-600 focus:text-white">
+        Events
+    </button>
+    </div>
    
+  </div>
 </template>
