2dffe79ddaf5557fb4823273325b6a39cb011c1d
kaandurak <kaandurak3d@gmail.com>
2026-01-15 21:06:37 +0100

    navbar fix

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,25 +3,25 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <!-- Logo Row -->
+
   <div class="bg-white grid grid-cols-12 p-5 shadow-2xl mb-3">
     <div class="w-60 h-10 col-start-2 col-span-2">
       <img class="w-auto h-10" src="../assets/logo_pxl_smart-ict.svg" alt="pxl-logo">
     </div>
   </div>
 
-  <!-- Navigation Buttons -->
+
   <div class="grid grid-cols-12">
     <div class="col-start-2 col-span-10 flex justify-between">
       
-      <!-- Communities button -->
+      
       <RouterLink to="/" class="w-[49%]">
         <button class="bg-white border-gray-400 border-5 rounded-2xl text-black w-full p-1 focus:bg-gray-600 focus:text-white text-base">
           Communities
         </button>
       </RouterLink>
 
-      <!-- Events button -->
+      
       <RouterLink to="/eventview" class="w-[49%]">
         <button class="bg-white border-gray-400 border-5 rounded-2xl text-black w-full p-1 focus:bg-gray-600 focus:text-white text-base">
           Events
